aee375ff8df5d4d86912b321cf54c4accb07fe6f
Student <example@example.com>
2019-03-29 10:02:39 -0700

    add top-ad component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TopAddComponent } from './top-add/top-add.component';
+import { TopAdComponent } from './top-ad/top-ad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    TopAddComponent,
+    TopAdComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/top-ad/top-ad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-ad',
+  templateUrl: './top-ad.component.html',
+  styleUrls: ['./top-ad.component.scss']
+})
+export class TopAdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
